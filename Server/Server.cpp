e832fc2354c813cc10a313632ec63a738d609c5c
DBCKS97 <wonyu1997@naver.com>
2025-12-22 09:52:50 +0900

    Server select 구조

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -13,22 +13,85 @@ using namespace std;
 
 int main()
 {
-	WSAData wsaData;
+	WSAData WsaData;
 
-	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	WSAStartup(MAKEWORD(2, 2), &WsaData);
 
-	hostent* host = gethostbyname("google.com");
+	SOCKET ListenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-	if (host)
+	SOCKADDR_IN ListenSockAddr;
+	memset(&ListenSocket, 0, sizeof(ListenSockAddr));
+	ListenSockAddr.sin_family = PF_INET;
+	ListenSockAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+	ListenSockAddr.sin_port = htons(17777);
+
+	bind(ListenSocket, (sockaddr*)&ListenSockAddr, sizeof(ListenSockAddr));
+	listen(ListenSocket, 10);
+
+	FD_SET ReadSocketList;
+	FD_ZERO(&ReadSocketList);
+
+	FD_SET(ListenSocket, &ReadSocketList);
+
+	TIMEVAL TiemOut{ 0, 100 };
+
+	while (true)
 	{
-		cout << host->h_name << endl;
+		FD_SET CopyReadSocketList = ReadSocketList;
+		int ChangeCount = select(0, &CopyReadSocketList, nullptr, nullptr, &TiemOut);
+
+		if (ChangeCount == 0)
+		{
+			cout << "Wait" << endl;
+			continue;
+		}
+
+		for (int i = 0; i < (int)ReadSocketList.fd_count; ++i)
+		{
+			SOCKET SelectSocket = ReadSocketList.fd_array[i];
+
+			if (FD_ISSET(SelectSocket, &CopyReadSocketList))
+			{
+				if (SelectSocket == ListenSocket)
+				{
+					// 연결요청
+					SOCKADDR_IN ClientSockAddr;
+					memset(&ClientSockAddr, 0, sizeof(ClientSockAddr));
+					int ClientSockAddrLength = sizeof(ClientSockAddr);
+
+					SOCKET ClientSocket = accept(ListenSocket, (sockaddr*)&ClientSockAddr, &ClientSockAddrLength);
+
+					FD_SET(ClientSocket, &ReadSocketList);
+
+					cout << "Client connected " << inet_ntoa(ClientSockAddr.sin_addr) << endl;
+				}
+				else
+				{
+					// 데이터 수신
+					char Buffer[1024] = { 0 };
+					int RecvBytes = recv(SelectSocket, Buffer, sizeof(Buffer), 0);
+					if (RecvBytes == 0)
+					{
+						cout << "Client disconnect : " << SelectSocket << endl;
+						FD_CLR(SelectSocket, &ReadSocketList);
+						closesocket(SelectSocket);
+					}
+					else if(RecvBytes < 0)
+					{
+						cout << "Client error disconnect : " << SelectSocket << endl;
+						FD_CLR(SelectSocket, &ReadSocketList);
+						closesocket(SelectSocket);
+					}
+
+					cout << "Client send : " << Buffer << endl;
+
+					int SendBytes = send(SelectSocket, Buffer, RecvBytes, 0);
+				}
+			}
+		}
 	}
 
-	for (int i = 0; host->h_aliases[i]; ++i)
-	{
-		cout << host->h_aliases[i] << endl;
-	}
-	
+	closesocket(ListenSocket);
 
 	WSACleanup();
 
